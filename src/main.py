67be5ff9100701269c8f54a3f74472d282a8351a
- IIRESODH - <webmaster@iiresodh.org>
2025-09-16 10:08:20 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -74,8 +74,8 @@ class PDF(FPDF):
 
 @app.post("/analyze-documents")
 async def analyze_documents(files: List[UploadFile] = File(...), instructions: str = Form(...)):
-    if len(files) > 10:
-        raise HTTPException(status_code=400, detail="Se permite un máximo de 10 archivos.")
+    if len(files) > 3:
+        raise HTTPException(status_code=400, detail="Se permite un máximo de 3 archivos.")
     
     # --- INICIO DE LA MODIFICACIÓN ---
     
